439e961f00965bbb811e90b2cd226a4778ac92df
ksh1710 <kshitizagarwal1710@gmail.com>
2023-07-16 17:40:29 +0530

    bottom nav bar added

--- app/src/main/java/com/example/innogeeks_team_project/MainActivity.kt
+++ app/src/main/java/com/example/innogeeks_team_project/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.innogeeks_team_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myNavController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFrag = supportFragmentManager.findFragmentById(R.id.mainFrag) as NavHostFragment
+        myNavController = navHostFrag.navController
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+        setupWithNavController(bottomNavBar,myNavController)
     }
 }
