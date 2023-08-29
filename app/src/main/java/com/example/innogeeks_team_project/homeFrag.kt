dd18f3a3790f5157a2c59effe5245f8d82cc5d3b
ksh1710 <kshitizagarwal1710@gmail.com>
2023-08-29 13:22:27 +0530

    minor stuff

--- app/src/main/java/com/example/innogeeks_team_project/homeFrag.kt
+++ app/src/main/java/com/example/innogeeks_team_project/homeFrag.kt
@@ -7,10 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 
 class homeFrag : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-    }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
