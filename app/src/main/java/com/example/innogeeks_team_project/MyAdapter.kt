c89e499f01cffffc53cf6b421af3b1a4fa2e0fa8
ksh1710 <kshitizagarwal1710@gmail.com>
2023-08-29 13:24:34 +0530

    minor stuff

--- app/src/main/java/com/example/innogeeks_team_project/MyAdapter.kt
+++ app/src/main/java/com/example/innogeeks_team_project/MyAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.innogeeks_team_project
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +9,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.shape.Shapeable
 
-class MyAdapter(private val foodList: ArrayList<food>) : RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+class MyAdapter(private val foodList: ArrayList<food>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
 
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
       val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent,false)
         return MyViewHolder(itemView)
@@ -19,8 +22,8 @@ class MyAdapter(private val foodList: ArrayList<food>) : RecyclerView.Adapter<My
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = foodList[position]
-        holder.banana.setImageResource(currentItem.)
-        holder.textView2.text = currentItem.
+        holder.banana.setImageResource(currentItem.foodImage)
+        holder.textView2.text = currentItem.foodName
     }
 
     override fun getItemCount(): Int {
